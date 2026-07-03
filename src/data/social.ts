a657bb80dd3becad8acdbf94088ca76a7cfa7e3f
Harsha Bikkavilli <harshabikkavilli@gmail.com>
2026-07-03 11:13:52 -0700

    Update NACC social links

--- src/data/social.ts
+++ src/data/social.ts
@@ -2,12 +2,12 @@ export const socialLinks = [
   {
     label: 'Instagram',
     icon: 'instagram',
-    href: 'https://www.instagram.com/',
+    href: 'https://www.instagram.com/northamericacricketconference/',
   },
   {
     label: 'Facebook',
     icon: 'facebook',
-    href: 'https://www.facebook.com/',
+    href: 'https://www.facebook.com/profile.php?id=61573313459360',
   },
   {
     label: 'YouTube',
